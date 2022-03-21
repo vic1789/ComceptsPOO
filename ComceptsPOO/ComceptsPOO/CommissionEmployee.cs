bc260385364768e71bdde9988b2e523b2950fe02
vic1245 <vic12448@hotmail.com>
2022-03-21 08:32:29 -0500

    Employee, SalaryEmployee & CommisionEmployee finish

---
+++ ComceptsPOO/ComceptsPOO/CommissionEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComceptsPOO
+{
+    public class CommissionEmployee : Employee
+    {
+        public float CommisionPercentaje { get; set; }
+
+        public decimal Sales { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)CommisionPercentaje;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission percentaje: {$"{CommisionPercentaje:P2}",18}"+
+                $"\n\tSales................: {$"{Sales:C2}",18}"+
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}",18}";
+        }
+    }
+}
--- ComceptsPOO/ComceptsPOO/Program.cs
+++ ComceptsPOO/ComceptsPOO/Program.cs
@@ -3,17 +3,29 @@ using ComceptsPOO;
 
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
-
-try
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 31));
-
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName =  "Morales",
+    BirthDate = new Date(1990,5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
+Employee employee2 = new CommissionEmployee()
 {
+    Id = 2010,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 15),
+    IsActive = true,
+    Sales = 32000000M,
+    CommisionPercentaje = 0.03F,
+};
+Console.WriteLine(employee2);
 
-    Console.WriteLine(error.Message);
-}
+
 
